3c742d03149d80e2b8d343535e1ca3d2fc06032b
Yash <168829659+Yash182002@users.noreply.github.com>
2026-01-22 19:29:34 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,26 +1,7 @@
-@import "tailwindcss";
+@@ -0,0 +1,6 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true
+};
 
-:root {
-  --background: #ffffff;
-  --foreground: #171717;
-}
-
-@theme inline {
-  --color-background: var(--background);
-  --color-foreground: var(--foreground);
-  --font-sans: var(--font-inter-sans);
-  --font-mono: var(--font-inter-mono);
-}
-
-@media (prefers-color-scheme: dark) {
-  :root {
-    --background: #0a0a0a;
-    --foreground: #ededed;
-  }
-}
-
-body {
-  background: var(--background);
-  color: var(--foreground);
-  font-family: Arial, Helvetica, sans-serif;
-}
+module.exports = nextConfig;
