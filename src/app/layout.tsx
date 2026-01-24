e4c1f1b8c0213b9cf57ddb9ab2b97d16fd64c00b
Yash <168829659+Yash182002@users.noreply.github.com>
2026-01-24 17:43:04 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,20 @@
+import Navbar from "@/components/Navbar";
+
+export default function RootLayout({
+  children
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <Navbar />
+        {children}
+      </body>
+    </html>
+  );
+}
+
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
