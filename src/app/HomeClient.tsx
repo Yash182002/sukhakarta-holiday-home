a764e1cf9547f1e3e8171128f331aa6bdd4525b0
Yash <168829659+Yash182002@users.noreply.github.com>
2026-01-24 18:08:01 +0530

    Create HomeClient.tsx

---
+++ src/app/HomeClient.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function HomeClient() {
+  return (
+    <main>
+      <h1>Sukhakarta Holiday Home</h1>
+      {/* Move all client-side logic here */}
+    </main>
+  );
+}
