9021d4600168df41abb2e7794ce6e5b2c820222b
Yash <168829659+Yash182002@users.noreply.github.com>
2026-01-25 15:56:42 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -86,7 +86,7 @@ export default function Navbar() {
               src="/logo.png"
               alt="Sukhakarta Holiday Home"
               className="logo-image"
-              style={{ height: "48px", width: "auto", animation: "none" }}
+              style={{ height: "40px", width: "auto", animation: "none" }}
             />
           </div>
 
