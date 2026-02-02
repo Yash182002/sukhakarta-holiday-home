6cbcda9b82eef6092768b79797a9102203a4728f
Yash <168829659+Yash182002@users.noreply.github.com>
2026-02-02 23:22:46 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -121,7 +121,6 @@ export default function Footer() {
               </div>
             </div>
           </div>
-        </div>
 
         {/* Bottom Bar */}
         <div className="footer-bottom">
