c25df11bb994a25d03385da0cc43c6bf3514a6b3
Yash182002 <yashpatil182002@gmail.com>
2026-07-16 15:54:53 +0530

    Remove stray file and secret from test script

--- test-cron.js
+++ test-cron.js
@@ -1,6 +1,6 @@
 fetch("http://localhost:3000/api/cron/vitals", {
   headers: {
-    Authorization: "Bearer 41b82ca6c907cb81338e6c72d62a3eb71cf7314ee8f32ca413e96c31abd6623a"
+    Authorization: "Bearer " + process.env.CRON_SECRET
   }
 })
   .then(res => res.json())
