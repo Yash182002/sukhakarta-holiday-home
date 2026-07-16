d17070b19df2977ec3b1a520e3cd6617ce280ac7
Yash <168829659+Yash182002@users.noreply.github.com>
2026-07-16 22:54:07 +0530

    Update API URL from localhost to production

--- test-cron.js
+++ test-cron.js
@@ -1,4 +1,4 @@
-fetch("http://localhost:3000/api/cron/vitals", {
+fetch("https://sukhakartaholidayhome.in/api/cron/vitals, {
   headers: {
     Authorization: "Bearer " + process.env.CRON_SECRET
   }
