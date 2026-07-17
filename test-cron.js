11c5404c2bd9377ed4b1cf08633964b8665eebc9
Yash182002 <yashpatil182002@gmail.com>
2026-07-17 12:06:21 +0530

    update test cron secret

--- test-cron.js
+++ test-cron.js
@@ -1,6 +1,6 @@
 fetch("https://sukhakartaholidayhome.in/api/cron/vitals", {
   headers: {
-    Authorization: "Bearer YOUR_NEW_SECRET_HERE"
+    Authorization: "f6d7b98a4945b0e485d6b24999ad296ebfc03634da6f45b06475823efe266cb9"
   }
 })
   .then(res => res.json())
